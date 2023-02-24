a62b8766db7953e5622dd57fae73f363fec34c50
hokyunrhee <hokyun.rhee@gmail.com>
2023-02-24 15:59:04 +0900

    refactor: GameResults UI 개선

--- src/components/Game/Game.test.js
+++ src/components/Game/Game.test.js
@@ -1,20 +1,35 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, within } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
 import Game from "./Game";
+import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
 
 describe("Game", () => {
-  it("displays guesses submitted", async () => {
+  it(`displays ${NUM_OF_GUESSES_ALLOWED} rows and each row has 5 spans`, async () => {
+    render(<Game />);
+
+    const rows = screen.getAllByRole("group");
+    expect(rows).toHaveLength(NUM_OF_GUESSES_ALLOWED);
+
+    rows.forEach((row) => {
+      expect(within(row).getAllByRole("img")).toHaveLength(5);
+    });
+  });
+
+  it("displays word submitted", async () => {
     const user = userEvent.setup();
 
     render(<Game />);
 
     await user.type(screen.getByRole("textbox"), "apple{enter}");
 
-    expect(await screen.findByText("APPLE")).toBeInTheDocument();
+    const row1 = screen.getByLabelText("row-1");
+    expect(within(row1).getByLabelText("A")).toBeInTheDocument();
+    expect(within(row1).getAllByLabelText("P")).toHaveLength(2);
+    expect(within(row1).getByLabelText("L")).toBeInTheDocument();
+    expect(within(row1).getByLabelText("E")).toBeInTheDocument();
 
-    await user.type(screen.getByRole("textbox"), "agent{enter}");
-
-    expect(await screen.findByText("AGENT")).toBeInTheDocument();
+    const row2 = screen.getByLabelText("row-2");
+    expect(within(row2).getAllByLabelText("empty")).toHaveLength(5);
   });
 });
--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -1,11 +1,35 @@
+import { range } from "../../utils";
+import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
+
 export const GuessResults = ({ guesses }) => {
+  const rows = range(0, NUM_OF_GUESSES_ALLOWED);
+
   return (
     <div className="guess-results">
-      {guesses.map(({ id, word }) => (
-        <p key={id} className="guess">
-          {word}
-        </p>
-      ))}
+      {rows.map((index) => {
+        const { id = index, word = "" } = guesses[index] || {};
+        const labelText = `row-${index + 1}`;
+
+        return <Guess key={id} word={word} labelText={labelText} />;
+      })}
     </div>
+  );
+};
+
+const Guess = ({ word, labelText }) => {
+  const characters = word.split("");
+
+  return (
+    <p className="guess" role="group" aria-label={labelText}>
+      {range(0, 5).map((index) => {
+        const labelText = characters[index] ?? "empty";
+
+        return (
+          <span key={index} className="cell" role="img" aria-label={labelText}>
+            {characters[index]}
+          </span>
+        );
+      })}
+    </p>
   );
 };
